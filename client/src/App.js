33536bfbc90c23fc3efef7557144b051e20cb9de
Maddy <madisonschaaf48@gmail.com>
2022-06-07 13:44:40 -0600

    set up routers

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,11 @@
 import React from 'react';
 import {ApolloProvider, ApolloClient, InMemoryCache, createHttpLink } from '@apollo/client'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Login from './pages/Login'
+import NoMatch from './pages/NoMatch'
+import SingleThought from './pages/SingleThought'
+import Profile from './pages/Profile'
+import Signup from './pages/Signup'
 
 import Header from './components/Header';
 import Footer from './components/Footer';
@@ -18,13 +24,22 @@ const client = new ApolloClient({
 function App() {
   return (
     <ApolloProvider client={client}>
+      <Router>
     <div className='flex-column justify-flex-start min-100-vh'>
       <Header />
       <div className='container'>
-        <Home />
+        <Routes>
+          <Route path ='/' element={<Home />} />
+          <Route path="/login" element={<Login/>}/>
+          <Route path="/signup" element={<Signup/>}/>
+          <Route path ="/profile/:username" element ={<Profile/>}/>
+          <Route path ="/thought/:id" element ={<SingleThought/>}/>
+          <Route path="*"element={<NoMatch/>}/>
+        </Routes>
       </div>
       <Footer />
     </div>
+    </Router>
     </ApolloProvider>
   );
 }
