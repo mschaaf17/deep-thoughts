bb87be427e99b9e6dfa40b979470766d6c549c4d
Maddy <madisonschaaf48@gmail.com>
2022-06-06 20:05:34 -0600

    connected servers

--- server/server.js
+++ server/server.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const { ApolloServer } = require('apollo-server-express');
+const { authMiddleware } = require('./utils/auth')
+const path = require('path')
 
 const { typeDefs, resolvers } = require('./schemas');
 const db = require('./config/connection');
@@ -7,7 +9,8 @@ const db = require('./config/connection');
 const PORT = process.env.PORT || 3001;
 const server = new ApolloServer({
   typeDefs,
-  resolvers
+  resolvers,
+  context: authMiddleware
 });
 
 const app = express();
@@ -19,6 +22,15 @@ app.use(express.json());
 const startApolloServer = async (typeDefs, resolvers) => {
   await server.start();
   server.applyMiddleware({ app });
+
+  //Serve up static assets
+  if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, '../client/build')))
+  }
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../client/build/index.html'))
+  })
   
   db.once('open', () => {
     app.listen(PORT, () => {
